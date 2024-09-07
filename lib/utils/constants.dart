839efa5615a74d135c59df435a2a6aa386ab975f
Agung Jaya <agung.jaya@ist.id>
2024-09-07 16:17:09 +0700

    feat : set constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+const Color mainColor = Color(0xFF282C33);
+const Color white = Color(0xFFFFFFFF);
+const Color gray = Color(0xFFABB2BF);
+const Color purple = Color(0xFFC778DD);
+const Color darkGray = Color(0xFF3B4049);
