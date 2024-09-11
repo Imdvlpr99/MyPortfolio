99711ae4fe4dfdcbd68df088f2faa039cb3051a5
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-09-12 04:27:56 +0700

    feat : create works and about base screen

---
+++ lib/ui/about/about_base.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AboutBase extends StatelessWidget {
+  const AboutBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
---
+++ lib/ui/works/works_base.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class WorksBase extends StatelessWidget {
+  const WorksBase({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
