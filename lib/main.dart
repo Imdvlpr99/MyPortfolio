7d5d936b06a5d9cad0d20c7a4b1a7880cdb2b31f
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-09-13 22:50:49 +0700

    feat : set url strategy

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:MyPortfolio/utils/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'utils/firebase_options.dart';
 
@@ -11,6 +12,7 @@ void main() {
   Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  setPathUrlStrategy();
   runApp(const MyApp());
 }
 
