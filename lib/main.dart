b5b2f0f8f47ff47d28d4399c3a14463b07189d1b
Imdvlpr99 <imdvlpr.agency@gmail.com>
2024-09-13 22:50:30 +0700

    fix : remove splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:MyPortfolio/screen/splash.dart';
 import 'package:MyPortfolio/utils/constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
